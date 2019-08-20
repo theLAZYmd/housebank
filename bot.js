f78e64538a31e88a8aec91595cd91e72b6e5dce7
theLAZYmd <lazymeep+github@gmail.com>
2019-08-20 22:11:38 +0100

    Modernised logging system with winston

--- bot.js
+++ bot.js
@@ -16,30 +16,31 @@ client.on('ready', () => {
 	bouncerbot = client.users.get('365933164725534722') || {
 		id: ''
 	};
-	Logger.log(bouncerbot.id ? `Noticed bot user ${bouncerbot.tag} in ${Date.now() - reboot}ms` : 'Bouncer#8585 is not online!');
+	Logger.info(bouncerbot.id ? `Noticed bot user ${bouncerbot.tag} in ${Date.now() - reboot}ms` : 'Bouncer#8585 is not online!');
 	nadekobot = client.users.get('116275390695079945') || {
 		id: ''
 	};
-	Logger.log(nadekobot.id ? `Noticed bot user ${nadekobot.tag} in ${Date.now() - reboot}ms` : 'Nadeko#6685 is not online!');
-	Logger.log('Beep bloop. It\'s secret showtime.');
+	Logger.info(nadekobot.id ? `Noticed bot user ${nadekobot.tag} in ${Date.now() - reboot}ms` : 'Nadeko#6685 is not online!');
+	Logger.info('Beep bloop. It\'s secret showtime.');
 });
 
-process.on('unhandledRejection', (error) => {
-	Logger.log('error', 'Uncaught Promise Error:', error);
-});
-
+process.on('unhandledRejection', Logger.error);
 client.on('error', Logger.error);
 
 client.on('message', (message) => {
-	if (message.author.id === client.user.id) return;
-	if (message.channel.type === 'dm' || message.channel.type === 'group')  return;
-	if (ids.includes(message.author.id)) {
-		if (message.content.startsWith('.award') || message.content.startsWith('.take')) {
-			message.channel.send(message.content);
-			message.delete();
+	try {
+		if (message.author.id === client.user.id) return;
+		if (message.channel.type === 'dm' || message.channel.type === 'group')  return;
+		if (ids.includes(message.author.id)) {
+			if (message.content.startsWith('.award') || message.content.startsWith('.take')) {
+				message.channel.send(message.content);
+				message.delete();
+			}
 		}
+		if (message.author.bot) Trivia.run(message);
+	} catch (e) {
+		if (e) Logger.error(e);
 	}
-	if (message.author.bot) Trivia.run(message);
 });
 
 client.login(process.env.TOKEN); //token
